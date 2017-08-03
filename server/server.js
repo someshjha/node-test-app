23d0d41d112140772057a528106accef78f8e0e4
Somesh Jha <someshj@ca.ibm.com>
2017-08-03 15:53:51 -0300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+
+var app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
+
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
+
+module.exports = {app};
+
